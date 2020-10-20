cc1fd9eb2a3091d0d3f53dfbdf475e34189b625f
Natalia <natajenkins@hotmail.com>
2020-10-20 13:05:54 -0500

    my_count

--- enumerables.rb
+++ enumerables.rb
@@ -1,121 +1,136 @@
+# rubocop:disable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/ModuleLength
+
 module Enumerable
+  # my_each
+  def my_each()
+    length.times do |n|
+      yield(self[n])
+    end
+    self
+  end
 
-  #my_each
-    def my_each(&block)
-      self.length().times do |n|
-        yield(self[n])
-      end
-      return self
+  # my_each_with_index
+  def my_each_with_index()
+    length.times do |i|
+      yield(self[i], i)
     end
+    self
+  end
 
-    #my_each_with_index
-    def my_each_with_index(&block)
-        self.length().times do |i|
-          yield(self[i],i)
-        end
-        return self
-    end
-
-    #my_select
-    def my_select(&block)
-        self.my_each do |elem|
-            elem if yield(elem) == true
-        end
-    end
-
-    #my_all?
-    def my_all?(arg = nil)
-      if block_given? # con bloque sin argumento
-        self.each do |item|
-          if yield(item) == false
-            return false
-          end
-        end
-      else # con argumento
-        if arg.class == Regexp
-          self.each do |item|
-            return false if arg.match?(item) == false
-          end
-        elsif arg.class == Class
-          self.each do |item|
-            return false if item.is_a?(arg) == false
-          end
-        elsif arg == nil
-          self.each do |item|
-            return false if !item
-          end
-        end
-      end
-      true
-    end
-    
-    #my_any?
-    def my_any?(arg = nil)
-      if block_given? # con bloque sin argumento
-        self.each do |item|
-          if yield(item) == true
-            return true
-          end
-        end
-      else # con argumento
-        if arg.class == Regexp
-          self.each do |item|
-            return true if arg.match?(item) == true
-          end
-        elsif arg.class == Class
-          self.each do |item|
-            return true if item.is_a?(arg) == true
-          end
-        elsif arg == nil
-          self.each do |item|
-            return true if item
-          end
-        end
-      end
-      false
-    end
-  
-    #my_none?
-    def my_none?(arg = nil)
-      if block_given? # con bloque sin argumento
-        self.each do |item|
-          if yield(item) == true
-            return false
-          end
-        end
-      else # con argumento
-        if arg.class == Regexp
-          self.each do |item|
-            return false if arg.match?(item) == true
-          end
-        elsif arg.class == Class
-          self.each do |item|
-            return false if item.is_a?(arg) == true
-          end
-        elsif arg == nil
-          self.each do |item|
-            return false if item
-          end
-        end
-      end
-      true
+  # my_select
+  def my_select(&block)
+    my_each do |elem|
+      elem if yield(elem) == true
     end
   end
 
-    #my_count
-    def my_count
-    end
-
-    #my_map
-    def my_map(&block)
-      result = Array.new
-      self.my_each do |n|
-        yield(n)
+  # my_all?
+  def my_all?(arg = nil)
+    if block_given? # con bloque sin argumento
+      each do |item|
+        return false if yield(item) == false
       end
-      return result
+    else # con argumento
+      if arg.class == Regexp
+        each do |item|
+          return false if arg.match?(item) == false
+        end
+      elsif arg.class == Class
+        each do |item|
+          return false if item.is_a?(arg) == false
+        end
+      elsif arg.nil?
+        each do |item|
+          return false unless item
+        end
+      end
     end
+    true
+  end
 
-    #my_inject
-    def my_inject
+  # my_any?
+  def my_any?(arg = nil)
+    if block_given? # con bloque sin argumento
+      each do |item|
+        return true if yield(item) == true
+      end
+    else # con argumento
+      if arg.class == Regexp
+        each do |item|
+          return true if arg.match?(item) == true
+        end
+      elsif arg.class == Class
+        each do |item|
+          return true if item.is_a?(arg) == true
+        end
+      elsif arg.nil?
+        each do |item|
+          return true if item
+        end
+      end
     end
+    false
+  end
+
+  # my_none?
+  def my_none?(arg = nil)
+    if block_given? # con bloque sin argumento
+      each do |item|
+        return false if yield(item) == true
+      end
+    else # con argumento
+      if arg.class == Regexp
+        each do |item|
+          return false if arg.match?(item) == true
+        end
+      elsif arg.class == Class
+        each do |item|
+          return false if item.is_a?(arg) == true
+        end
+      elsif arg.nil?
+        each do |item|
+          return false if item
+        end
+      end
+    end
+    true
+  end
+
+  # my_count
+  def my_count(*num)
+    array = to_a
+    count = 0
+    if num.length.zero?
+      i = 0
+      while i < array.length
+        if block_given?
+          count += 1 if yield array[i]
+        else
+          return array.length
+        end
+        i += 1
+      end
+    elsif num.length == 1
+      i = 0
+      while i < array.length
+        count += 1 if num[0] == array[i]
+        i += 1
+      end
+    end
+    count
+  end
+
+  # my_map
+  def my_map(&block)
+    result = []
+    my_each do |n|
+      yield(n)
+    end
+    result
+  end
+
+  # my_inject
+  def my_inject(*_args)
+   
+  end
 end
