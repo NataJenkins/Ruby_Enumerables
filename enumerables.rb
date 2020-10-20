6f8e2a0636b21bc32217fc30583fa77945c61c37
Natalia <natajenkins@hotmail.com>
2020-10-20 09:15:01 -0500

    My all & My none

--- enumerables.rb
+++ enumerables.rb
@@ -24,37 +24,83 @@ module Enumerable
     end
 
     #my_all?
-    def my_all?(&block)
+    def my_all?(arg = nil)
+      if block_given? # con bloque sin argumento
+        self.each do |item|
+          if yield(item) == false
+            return false
+          end
+        end
+      else # con argumento
+        if arg.class == Regexp
+          self.each do |item|
+            return false if arg.match?(item) == false
+          end
+        elsif arg.class == Class
+          self.each do |item|
+            return false if item.is_a?(arg) == false
+          end
+        elsif arg == nil
+          self.each do |item|
+            return false if !item
+          end
+        end
+      end
+      true
     end
     
-
     #my_any?
-    if block_given? # con bloque sin argumento
-      self.each do |item|
-        if yield(item) == true
-          return true
+    def my_any?(arg = nil)
+      if block_given? # con bloque sin argumento
+        self.each do |item|
+          if yield(item) == true
+            return true
+          end
+        end
+      else # con argumento
+        if arg.class == Regexp
+          self.each do |item|
+            return true if arg.match?(item) == true
+          end
+        elsif arg.class == Class
+          self.each do |item|
+            return true if item.is_a?(arg) == true
+          end
+        elsif arg == nil
+          self.each do |item|
+            return true if item
+          end
         end
       end
-    else # con argumento
-      if arg.class == Regexp
-        self.each do |item|
-          return true if arg.match?(item) == true
-        end
-      elsif arg.class == Class
-        self.each do |item|
-          return true if item.is_a?(arg) == true
-        end
-      elsif arg == nil
-        self.each do |item|
-          return true if item
-        end
-      end
+      false
     end
-    false
-  end
+  
     #my_none?
-    def my_none?
+    def my_none?(arg = nil)
+      if block_given? # con bloque sin argumento
+        self.each do |item|
+          if yield(item) == true
+            return false
+          end
+        end
+      else # con argumento
+        if arg.class == Regexp
+          self.each do |item|
+            return false if arg.match?(item) == true
+          end
+        elsif arg.class == Class
+          self.each do |item|
+            return false if item.is_a?(arg) == true
+          end
+        elsif arg == nil
+          self.each do |item|
+            return false if item
+          end
+        end
+      end
+      true
     end
+  end
 
     #my_count
     def my_count
